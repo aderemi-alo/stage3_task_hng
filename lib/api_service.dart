40718cfc950aa97bbc8223705b28cc66fc8462a9
FlutterBugs <aloaderemi1@gmail.com>
2022-11-11 17:44:43 +0100

    sorted the list of countries

---
+++ lib/api_service.dart
@@ -0,0 +1,23 @@
+import 'country_model.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class APIService {
+  Future<List<CountryModel>> getCountries() async {
+    var client = http.Client();
+
+    var url = Uri.parse("https://restcountries.com/v3.1/all");
+    var response = await client.get(url);
+    if (response.statusCode == 200) {
+      final json1 = json.decode(response.body);
+      final List<CountryModel> services =
+          json1.map<CountryModel>((e) => CountryModel.fromJson(e)).toList();
+      services.sort(
+        (a, b) => a.name!.common!.compareTo(b.name!.common!),
+      );
+      return services;
+    } else {
+      throw Exception("Failed to load");
+    }
+  }
+}
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:stage3_task_hng/country_model.dart';
-import 'country_model.dart';
+import 'api_service.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,13 +12,66 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<CountryModel>? countryModel;
+  late Future<List<CountryModel>> countries;
   var isLoaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  getData() async {
+    countries = APIService().getCountries();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(),
-    );
+        body: SafeArea(
+      minimum: const EdgeInsets.symmetric(horizontal: 24),
+      child: FutureBuilder<List<CountryModel>>(
+          future: countries,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            }
+            if (snapshot.hasData) {
+              if (snapshot.data == null) {
+                return const Text("List is empty");
+              }
+              return Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Explore",
+                        style: GoogleFonts.elsieSwashCaps(
+                            fontSize: 30, fontWeight: FontWeight.w700),
+                      ),
+                      const Icon(CupertinoIcons.sun_max)
+                    ],
+                  ),
+                  Flexible(
+                    child: ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: snapshot.data?.length,
+                        itemBuilder: (context, index) {
+                          return Row(
+                            children: [
+                              Text("${snapshot.data?[index].name?.common}"),
+                            ],
+                          );
+                        }),
+                  ),
+                ],
+              );
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            }
+            return const Text("erroe");
+          }),
+    ));
   }
 }
